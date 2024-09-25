993d15ddda2325e3e903cc71e3a17ab1e502d967
nageswara <nageswara@nthallap.myguest.virtualbox.org>
2024-09-25 10:39:14 +0530

    fixed drag and drop

--- drag_and_drop.py
+++ drag_and_drop.py
@@ -1,4 +1,5 @@
 from lrn_sel.basic import basic_setup
+from selenium.webdriver.common.by import By
 from selenium.webdriver import ActionChains
 import time
 
@@ -11,8 +12,8 @@ actions = ActionChains(driver)
 driver.switch_to.frame(0)
 
 time.sleep(2)
-draggable = driver.find_element_by_xpath("//div[@id='draggable']")
-droppable = driver.find_element_by_xpath("//div[@id='droppable']")
+draggable = driver.find_element(By.XPATH, "//div[@id='draggable']")
+droppable = driver.find_element(By.XPATH, "//div[@id='droppable']")
 
 
 #actions.drag_and_drop(dragable,dropable).perform()
